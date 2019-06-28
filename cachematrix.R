a0342f10f66cb47b5a539e2bb08c0ff5ab10d722
dyrl <51422791+dyrl@users.noreply.github.com>
2019-06-28 16:10:14 +0100

    Commit of edited cachematrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below function calculates and stores inverse of (an invertible) matrix. If the inverse has 
+## been calculated previously, function returns the cached value. 
+## If not, function calculates and caches the inverse.
 
-## Write a short comment describing this function
+## Function defines the following actions, to be called in later function:
+## Set matrix argument
+## Retrieve matrix argument
+## Cache inverse calculated
+## Retrieve inverse previously cached
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+              set <- function(y) {
+                      x <<- y
+                      inv <<- NULL
+              }
+              
+              get <- function() x
+              
+              setinv <- function(solve) inv <<- solve
+              
+              getinv <- function() if(exists("inv")) {inv} else {inv <- NULL}
+              
+              list(set = set, get = get,
+                   setinv = setinv,
+                   getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## For an input matrix, the function does the following:
+## 1. Checks if an inverse has previously been calculated and cached
+## 2. If 1 is TRUE, checks if the input matrix is consistent with cached inverse
+## If 1 & 2 TRUE, returns cached inverse
+## If 1 or 2 FALSE, calculates, returns, and caches inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    
+       inv <- x$getinv()
+ 
+       if(!is.null(inv)) {
+          if(exists("matr")) {
+            if(identical(matr, x$get())) {
+              message("getting cached data")
+              return(inv)
+            } 
+          }
+       }
+  
+ 
+       matr <<- x$get()
+       inv <- solve(matr, ...)
+       x$setinv(inv)
+       inv
+  
 }
